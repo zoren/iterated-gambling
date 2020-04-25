db63e26bde6066da7638880cc015c07093887681
Søren Sjørup <soren.sjorup@deondigital.com>
2020-04-25 20:08:39 +0200

    use Math.random, the central randomizer is terrible

--- iterative-gambling.mjs
+++ iterative-gambling.mjs
@@ -1,15 +1,9 @@
-import Random from "./Random.mjs";
-
-function run(seed, winBelow) {
-  const prng = new Random(seed);
+function run(winBelow) {
   const capitalFractionInvested = 0.5;
   var fortune = 1.0;
   var iterations = 0;
   while (true) {
-    const r = prng.getNextValue();
-    if (fortune == 0.0 || fortune == Infinity) {
-      return [fortune, iterations];
-    }
+    const r = Math.random();
     if (r < winBelow) {
       // win
       fortune = fortune + fortune * capitalFractionInvested;
@@ -17,25 +11,33 @@ function run(seed, winBelow) {
       // loose
       fortune = fortune * capitalFractionInvested;
     }
+    if (fortune == 0.0 || fortune == Infinity) {
+      return [fortune, iterations];
+    }
     iterations++;
   }
 }
 
-const numberOfRuns = 10;
+const numberOfRuns = 1000;
 
 function runMany(winBelow) {
+  console.log("win below", winBelow);
+  var brokeCount = 0,
+    richCount = 0;
   for (var i = 0; i < numberOfRuns; i++) {
-    const [fortune, iterations] = run(i, winBelow);
+    const [fortune] = run(winBelow);
     if (fortune == 0.0) {
-      console.log(`broke after ${iterations} iterations`);
+      brokeCount++;
     } else if (fortune == Infinity) {
-      console.log(`infinitely rich after ${iterations} iterations`);
-    } else {
-      console.log(`fortune ${fortune} after ${iterations} iterations`);
+      richCount++;
     }
   }
+  console.log(`broke ${brokeCount} rich ${richCount}`);
 }
 
-runMany(0.63091); // always go broke
-runMany(0.63093); // always win big
-runMany(0.63092); // stuck gambling for a long time
+runMany(0.63); // almost always go broke
+runMany(0.632); // always win big
+// https://en.wikipedia.org/wiki/List_of_mathematical_constants
+// Math.log(2)/Math.log(3) = 0.6309297535714575 Fractal dimension of the Cantor set
+// what have I gotten myself into...
+runMany(Math.log(2) / Math.log(3)); // stuck gambling for a long time
