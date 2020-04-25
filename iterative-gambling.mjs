4c0daf3c806b517587987f24acfb815fb2eca2f4
Søren Sjørup <soren.sjorup@deondigital.com>
2020-04-25 19:30:15 +0200

    gamble forever and and try for different ratios

--- iterative-gambling.mjs
+++ iterative-gambling.mjs
@@ -1,12 +1,15 @@
 import Random from "./Random.mjs";
 
-function run(seed, maxGamesPlayed) {
+function run(seed, winBelow) {
   const prng = new Random(seed);
-  const winBelow = 0.6309;
   const capitalFractionInvested = 0.5;
   var fortune = 1.0;
-  for (var i = 0; i < maxGamesPlayed; i++) {
+  var iterations = 0;
+  while (true) {
     const r = prng.getNextValue();
+    if (fortune == 0.0 || fortune == Infinity) {
+      return [fortune, iterations];
+    }
     if (r < winBelow) {
       // win
       fortune = fortune + fortune * capitalFractionInvested;
@@ -14,9 +17,25 @@ function run(seed, maxGamesPlayed) {
       // loose
       fortune = fortune * capitalFractionInvested;
     }
+    iterations++;
   }
-  return fortune;
 }
-for (var i = 0; i < 1000; i++) {
-  console.log(i, run(i, 1000000));
+
+const numberOfRuns = 10;
+
+function runMany(winBelow) {
+  for (var i = 0; i < numberOfRuns; i++) {
+    const [fortune, iterations] = run(i, winBelow);
+    if (fortune == 0.0) {
+      console.log(`broke after ${iterations} iterations`);
+    } else if (fortune == Infinity) {
+      console.log(`infinitely rich after ${iterations} iterations`);
+    } else {
+      console.log(`fortune ${fortune} after ${iterations} iterations`);
+    }
+  }
 }
+
+runMany(0.63091); // always go broke
+runMany(0.63093); // always win big
+runMany(0.63092); // stuck gambling for a long time
